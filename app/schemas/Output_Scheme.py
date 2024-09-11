7852572782c85ab83edd89be8c3ee695000b1f0e
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-11 18:18:35 -0300

    refactor(Removing): Removing all non-gitops files

--- app/schemas/Output_Scheme.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from app.schemas.custom_base_model import CustomBaseModel
-
-
-class PasswordOutput(CustomBaseModel):
-    message: str
-    data: List[dict]
--- test/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-
-@pytest.fixture
-def client():
-    return TestClient(app=app)
