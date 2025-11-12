6c08a679b3a54c727fe4cb37d1cff619c32b37df
Gabriel <115580274+gabszs@users.noreply.github.com>
2025-11-12 20:39:25 -0300

    Update app_routes.py

--- app/router/v1/app_routes.py
+++ app/router/v1/app_routes.py
@@ -11,23 +11,11 @@ async def health():
 
 @router.get("/debug")
 async def debug(request: Request):
-    headers = request.headers
     return {
-        "IPcountry": headers.get("CF-IPCountry"),
-        "IPcity": headers.get("CF-IPCity"),
-        "IPcontinent": headers.get("CF-IPContinent"),
-        "IPlatitude": headers.get("CF-IPLatitude"),
-        "IPlongitude": headers.get("CF-IPLongitude"),
-        "IPpostal_code": headers.get("CF-IPPostalCode"),
-        "IPtimezone": headers.get("CF-IPTimezone"),
-        "asOrganization": headers.get("CF-IPASNOrganization"),  # ISP
-        "country": headers.get("country"),
-        "city": headers.get("city"),
-        "continent": headers.get("continent"),
-        "region": headers.get("region"),
-        "regionCode": headers.get("regionCode"),
-        "timezone": headers.get("timezone"),
-        "longitude": float(headers.get("longitude")) if headers.get("CF-IPLongitude") else None,
-        "latitude": float(headers.get("latitude")) if headers.get("CF-IPLatitude") else None,
-        "postalCode": headers.get("postalCode"),
+        "headers": dict(request.headers),
+        "geo": {
+            "country": request.headers.get("cf-ipcountry"),
+            "city": request.headers.get("cf-ipcity"),
+            "isp": request.headers.get("cf-ipasnorganization")
+        }
     }
