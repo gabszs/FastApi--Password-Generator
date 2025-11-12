122f9969e9d73b21c16a9e7d4d4013ee6dfc0586
Gabriel <115580274+gabszs@users.noreply.github.com>
2025-11-12 20:16:21 -0300

    Update debug endpoint to include geo headers
    Enhanced the debug endpoint to return additional geographical information from request headers.

--- app/router/v1/app_routes.py
+++ app/router/v1/app_routes.py
@@ -11,4 +11,13 @@ async def health():
 
 @router.get("/debug")
 async def debug(request: Request):
-    return {"root_path": request.scope.get("root_path")}
+    headers = request.headers
+    return {
+        "country": headers.get("CF-IPCountry"),
+        "city": headers.get("CF-IPCity"),
+        "continent": headers.get("CF-IPContinent"),
+        "latitude": headers.get("CF-IPLatitude"),
+        "longitude": headers.get("CF-IPLongitude"),
+        "postal_code": headers.get("CF-IPPostalCode"),
+        "timezone": headers.get("CF-IPTimezone")
+    }
