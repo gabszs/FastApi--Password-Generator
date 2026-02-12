1fa5bc6e2a7d763ce11aee89fcd142d008d60f44
GabrielDeCarvalho <gnorberc@emeal.nttdata.com>
2026-02-12 12:11:10 -0300

    paswrod

--- app/router/v1/app_routes.py
+++ app/router/v1/app_routes.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from datetime import timezone
 
-import httpx
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import JSONResponse
@@ -34,33 +33,3 @@ async def debug(request: Request):
         },
         media_type="application/json; charset=utf-8",
     )
-
-
-@router.get("/password")
-async def get_password():
-    logger.info("Password fetch triggered")
-
-    url = "https://password.gabrielcarvalho.dev/v1/"
-    params = {
-        "password_length": 12,
-        "quantity": 1,
-        "has_punctuation": "true",
-    }
-
-    try:
-        async with httpx.AsyncClient(timeout=10) as client:
-            response = await client.get(url, params=params)
-            response.raise_for_status()
-            data = response.json()
-
-        password = data["data"][0]
-
-        return {
-            "status": "ok",
-            "password": password,
-            "timestamp": datetime.now(timezone.utc).isoformat(),
-        }
-
-    except httpx.HTTPError as e:
-        logger.error(f"Error fetching password: {str(e)}")
-        # raise HTTPException(status_code=502, detail="Failed to fetch external password")
